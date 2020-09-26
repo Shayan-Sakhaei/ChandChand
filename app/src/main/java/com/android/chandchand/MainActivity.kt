ccef74058bb65ded4a522f6d09a8bd02726ae948
Shayan <Shayansakhaeidev@gmail.com>
2020-09-26 19:03:59 +0330

    add Fonts, modify Colors and Styles, enable ViewBinding, add BottomNavigation and FragmentContainer to MainActivity.

--- app/src/main/java/com/android/chandchand/MainActivity.kt
+++ app/src/main/java/com/android/chandchand/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.android.chandchand
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import com.android.chandchand.databinding.ActivityMainBinding
+import com.android.chandchand.presentation.NavigationListener
+import com.android.chandchand.presentation.setupWithNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private var currentNavController: LiveData<NavController>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        val navGraphIds = listOf(
+            R.navigation.fixtures_graph,
+            R.navigation.leagues_graph
+        )
+
+        val controller = binding.bottomNavigation.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent,
+            navigationListener = this
+        )
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean =
+        currentNavController?.value?.navigateUp() ?: false
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+    }
+
 }
