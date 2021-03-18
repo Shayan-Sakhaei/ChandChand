0bc906b7a36b3c3ddd27eaad69cc1713baf4883f
Farhad Beigirad <f.beigirad@gmail.com>
2021-03-18 16:42:55 +0330

    remove redundant fixture-list fragments

--- app/src/main/java/com/android/chandchand/presentation/fixtures/FixturesPagerAdapter.kt
+++ app/src/main/java/com/android/chandchand/presentation/fixtures/FixturesPagerAdapter.kt
@@ -2,10 +2,6 @@ package com.android.chandchand.presentation.fixtures
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.android.chandchand.presentation.fixtures.daily.DayAfterTomorrowFixturesFragment
-import com.android.chandchand.presentation.fixtures.daily.TodayFixturesFragment
-import com.android.chandchand.presentation.fixtures.daily.TomorrowFixturesFragment
-import com.android.chandchand.presentation.fixtures.daily.YesterdayFixturesFragment
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
@@ -14,11 +10,7 @@ class FixturesPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) 
     override fun getItemCount(): Int = FixtureTabsModel.values().size
 
     override fun createFragment(position: Int): Fragment {
-        return when (FixtureTabsModel.values()[position]) {
-            FixtureTabsModel.Yesterday -> YesterdayFixturesFragment()
-            FixtureTabsModel.Today -> TodayFixturesFragment()
-            FixtureTabsModel.Tomorrow -> TomorrowFixturesFragment()
-            FixtureTabsModel.DayAfterTomorrow -> DayAfterTomorrowFixturesFragment()
-        }
+        val tab = FixtureTabsModel.values()[position]
+        return FixtureListFragment.newInstance(tab)
     }
 }
