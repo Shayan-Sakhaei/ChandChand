e1ed934bad57f566a3ef856e8dfe0c11d42ed050
Shayan Sakhaei <shayansakhaeidev@gmail.com>
2022-06-21 15:25:36 +0430

    migrate project root build.gradle(ChandChand) from Groovy to Kotlin Script (KTS).

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.android.gradle.pluginx)
+        classpath(libs.kotlin.gradle.pluginx)
+        classpath(libs.hilt.gradle.pluginx)
+        classpath(libs.navigation.gradle.pluginx)
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
