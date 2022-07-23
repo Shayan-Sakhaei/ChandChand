94000621760d880a42af8bd4f0694764a86e7d15
Shayan Sakhaei <shayansakhaeidev@gmail.com>
2022-07-23 16:54:58 +0430

    fix: unexpected return value from isSystemInDarkTheme() caused by overwriting AppCompatDelegate NightMode.

--- app/src/main/java/com/android/chandchand/presentation/theme/Themes.kt
+++ app/src/main/java/com/android/chandchand/presentation/theme/Themes.kt
@@ -3,9 +3,10 @@ package com.android.chandchand.presentation.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-val ChandChandLightColors = darkColors(
+val ChandChandLightColors = lightColors(
     primary = LightPrimary,
     primaryVariant = LightPrimaryVariant,
     secondary = LightSecondary,
